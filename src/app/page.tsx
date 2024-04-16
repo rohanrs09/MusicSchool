1289ca329491be44d51e25bed2f698cbeabb4242
rohanshelke <rohan_shelke_it@moderncoe.edu.in>
2024-04-16 19:55:44 +0530

    Revert "Sticky scroll"
    This reverts commit 45b3164e8f3d8041651bfe9bc5bb76d70cb67320.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
-import MusicSchoolTestimonials from "@/components/TestimonialCards";
-import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-    <HeroSection /> 
-    <FeaturedCourses /> 
-    <WhyChooseUs />
-    <MusicSchoolTestimonials />
+    <HeroSection />  
     </main>
   );
 }
